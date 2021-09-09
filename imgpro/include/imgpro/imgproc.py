d68b6c010c0bee09ec431b4023d1dac0879c01be
Harsh Agrawal <harshag37@gmail.com>
2021-09-09 10:00:44 +0530

    edited

--- imgpro/include/imgpro/imgproc.py
+++ imgpro/include/imgpro/imgproc.py
@@ -17,31 +17,33 @@ class image_feature:
         # subscribed Topic
         self.subscriber = rospy.Subscriber("/wamv/sensors/cameras/front_right_camera/image_raw",
             Image, self.callback,  queue_size = 10)
-        # self.result = cv2.VideoWriter('filename.avi', cv2.VideoWriter_fourcc(*'MJPG'),10, (360,640))
-        self.i=0
-    def imgpro(self,data):
-        scenePyr=data
-        if self.pyrDown > 0:
-            for i in range(self.pyrDown):
-                scenePyr = cv2.pyrDown(scenePyr)
-        imghsv=cv2.cvtColor(scenePyr,cv2.COLOR_BGR2HSV)
-        # height, width = scenePyr.shape[:2]
-        # self.result.write(scenePyr)
-        # imggray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        # kernel=np.array((5,5))
-        cv2.imwrite('kang'+str(self.i)+'.jpg',scenePyr)
-        self.i+=1
-        lower_red = np.array([0, 50, 50])
-        upper_red = np.array([10, 255, 255])
-        img_mask=cv2.inRange(imghsv, lower_red, upper_red)
-        cv2.imshow("Mask",img_mask)
-        cnts, hierarchy = cv2.findContours(img_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-        for cn in cnts:
-            cv2.fillPoly(scenePyr,pts=[cn],color=(0,255,0))
-        return scenePyr
+        self.color={"red":np.array([[0, 231, 94],[10, 255, 124],[0],[],[]]),
+        "blue":np.array([[119,244,89],[123,255,138],[0],[],[]]),
+        "black":np.array([[0,0,0],[0, 0, 9],[0],[],[]]),
+        "yellow":np.array([[27,226,93],[32, 255, 130],[0],[],[]]),
+        "green":np.array([[49, 244, 90],[64, 255, 119],[0],[],[]])}
+    def FindColor(self,img):
+        imghsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+        kernel=np.array((7,7))
+        for key ,value in self.color.items():
+            value[2][0]=0
+            img_mask=cv2.inRange(imghsv,np.array(value[0]),np.array(value[1]))
+            imgdilate=cv2.dilate(img_mask,kernel,iterations=30)
+            cnts_red, hierarchy = cv2.findContours(img_mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+            for cnt in cnts_red:
+                area=cv2.contourArea(cnt)
+                if area>500:
+                    value[2][0]+=1
+                    perimeter=cv2.arcLength(cnt,True)
+                    approx=cv2.approxPolyDP(cnt,0.02*perimeter,True)
+                    x,y,w,h=cv2.boundingRect(approx)
+                    value[2+value[2][0]]=x,y,w,h
+                    cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+                    cv2.putText(img, key,(int(x+w/2),int(y+h/2)),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,255),1)
+        return img
     def callback(self, ros_data):
         msg=self.bridge.imgmsg_to_cv2(ros_data,"bgr8")
-        msg=self.imgpro(msg)
+        msg=self.FindColor(msg)
         msg=self.bridge.cv2_to_imgmsg(msg,"bgr8")
         self.image_pub.publish(msg)
         
